d6f0bf96e9ef35876a3a76a33d10677c50433d11
Nintendocustom <41170567+Nintendocustom@users.noreply.github.com>
2019-01-29 21:44:52 +0100

    start

--- run-python.bat
+++ run-python.bat
@@ -1,29 +1,44 @@
 @ECHO OFF
 
-echo This script is able to start Miyamoto and/or download the
-echo latest spritedata and category data XMLs off of the internet.
-echo(
-echo Requirements:
-echo - Python 3+, installed to path (make sure the py command works)
-echo - PowerShell 3.0 (for the spritedata and categoryxml download), included
-echo   by default on Windows 10, for other versions of Windows,
-echo   you have to download the installer off of the internet.
-echo(
-echo Enjoy!
-
-:downloadthatstuff
+echo Dieses Skript kann Miyamoto starten und/oder die
+echo aktuelle Spritedaten und Kategoriedaten-XMLs aus dem Internet.
+echo.
+echo Anforderung:
+echo - Python 3+, installiert im Pfad (stellen Sie sicher, dass der Befehl py funktioniert)
+echo - PowerShell 3.0 (fuer den Download von Uebersetzungs Updates und anderen dateien), enthalten
+echo   standardmaessig unter Windows 10, fuer andere Versionen von Windows,
+echo   muessen Sie den Installer aus dem Internet herunterladen.
+echo.
+echo Geniessen Sie es!
 
 echo Downloading latest spritedata...
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Nintendocustom/Miyamoto/master/miyamotodata/spritedata.xml -OutFile miyamotodata/spritedata.xml"
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/tvfQWENS -OutFile miyamotodata/entrancetypes.txt"
 echo Done!
 
-:downloadthatxml
-
-@echo OFF
-echo Downloading latest cateogry data...
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Gota7/Miyamoto/master/miyamotodata/spritecategories.xml -OutFile miyamotodata/spritecategories.xml"
+echo Downloading latest music...
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/HQTWcxQP -OutFile miyamotodata/music.txt"
 echo Done!
-echo Starting Miyamoto!
+
+echo Downloading latest category...
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/usYcVZ9L -OutFile miyamotodata/spritecategories.xml"
+echo Done!
+
+echo Downloading latest spritedata...
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Gota7/Miyamoto/master/miyamotodata/spritedata.xml -OutFile miyamotodata/spritedata.xml"
+echo Done!
+
+echo Downloading latest ts1_descriptions...
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/BR6rEga3 -OutFile miyamotodata/ts1_descriptions.txt"
+echo Done!
+
+echo Downloading latest strings...
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/aALSGhRn -OutFile strings.py"
+echo Done!
+
 py miyamoto.py
-pause
+
+echo Downloading the latest version...
+powershell -Command "Invoke-WebRequest https://pastebin.com/raw/HT5pfYnY -OutFile run-python.bat"
+echo Done!
+
 EXIT
