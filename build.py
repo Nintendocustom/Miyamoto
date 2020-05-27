8e4b83c7d657c4aa95a69889c2658f910bd6f5fe
Nintendocustom <41170567+Nintendocustom@users.noreply.github.com>
2020-05-27 21:14:08 +0200

    Update build.py

--- build.py
+++ build.py
@@ -112,16 +112,19 @@ else:
 shutil.copy('license.txt', dir_)
 shutil.copy('README.md', dir_)
 print('>> Files copied!')
-import os
 import zipfile
+def zip(src, dst):
+    zf = zipfile.ZipFile("%s.zip" % (dst), "w", zipfile.ZIP_DEFLATED)
+    abs_src = os.path.abspath(src)
+    for dirname, subdirs, files in os.walk(src):
+        for filename in files:
+            absname = os.path.abspath(os.path.join(dirname, filename))
+            arcname = absname[len(abs_src) + 1:]
+            print('zipping %s as %s' % (os.path.join(dirname, filename),
+                                        arcname))
+            zf.write(absname, arcname)
+    zf.close()
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
-zipf = zipfile.ZipFile(PackageName + '.zip', 'w', zipfile.ZIP_DEFLATED)
-zipdir('dir_', zipf)
-zipf.close()
+zip(dir_, "Miyamoto")
 
 print('>> Miyamoto! has been frozen to %s !' % dir_)
